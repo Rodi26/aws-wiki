d28f287705ad448844b0280efdc862b7826f2aa1
Rodolphe Fontaine <30529563+Rodi26@users.noreply.github.com>
2022-07-07 14:30:13 +0200

    first run from git

--- main.tf
+++ main.tf
@@ -128,8 +128,6 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "devops"
-    Billable = "true"
   }
 }
 
@@ -174,8 +172,6 @@ resource "null_resource" "configure-cat-app" {
       "sudo chown -R ubuntu:ubuntu /var/www/html",
       "chmod +x *.sh",
       "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} ./deploy_app.sh",
-      "sudo apt -y install cowsay",
-      "cowsay Mooooooooooo!",
     ]
 
     connection {
