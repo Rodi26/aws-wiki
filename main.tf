407ed3a57dca6db726cdd6ec19bc92112e545fc1
Rodolphe Fontaine <30529563+Rodi26@users.noreply.github.com>
2022-08-16 17:15:25 +0200

    correction python 3

--- main.tf
+++ main.tf
@@ -166,7 +166,7 @@ resource "null_resource" "configure-cat-app" {
     inline = [
       "sudo apt-get update",
       "sudo apt-get install python3.6",
-      "sudo  python -m pip install virtualenv",
+      "sudo  python3 -m pip install virtualenv",
       "sleep 15",
       "sudo pip install -r ../requirements.txt",
       "sudo  mkdocs serve",
