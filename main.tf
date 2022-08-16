861af7969e992d553ab3ecca46be474b25d37b9f
Rodolphe Fontaine <30529563+Rodi26@users.noreply.github.com>
2022-08-16 18:38:02 +0200

    correction python 19

--- main.tf
+++ main.tf
@@ -143,7 +143,7 @@ resource "aws_instance" "wiki" {
 # Set up some environment variables for our script.
 # Add execute permissions to our scripts.
 # Run the deploy_app.sh script.
-resource "null_resource" "configure-cat-wiki" {
+resource "null_resource" "configure-app-wiki" {
   depends_on = [aws_eip_association.wiki]
 
   triggers = {
